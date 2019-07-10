50a6f91f1ef3f10bd17a126ff32e762ab7f07be6
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2019-07-09 21:54:07 -0300

    Referencing external tf module dependency to binbash leverage module url and tag

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "default_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  source = "git::git@github.com:binbashar/terraform-null-label.git?ref=v0.11.1"
   enabled    = "${var.enabled}"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
